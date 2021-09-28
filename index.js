9c8cd17e780394ea0cde82c98c5bfbb30e99ac9d
Priya Patel <83440686+Priya-Patel31@users.noreply.github.com>
2021-09-29 03:42:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+var market_price =  document.querySelector("#market-price");
+var quantity = document.querySelector("#quantity-stocks")
+var selling_price = document.querySelector("#selling-price");
+var btn = document.querySelector(".stock-button");
+var output = document.querySelector(".result");
+var form = document.querySelector(".form");
+var ctn = document.querySelector(".left-container");
+
+
+
+form.addEventListener("submit",(event) =>{
+    output.style.display="block";
+    event.preventDefault();
+    if(market_price.value ===0&& quantity === 0 && selling_price.value ===0)
+    {
+        output.innerText = "Input cannot be zero";
+    }else{
+        profitLoss(market_price,quantity,selling_price);
+    }
+
+});
+
+function profitLoss(market_price, quantity , selling_price){
+    if(market_price.value < selling_price.value )
+    {
+        var profit = (selling_price.value - market_price.value)*quantity.value;
+        var profitPercent = (profit/market_price.value)*100 ;
+       
+         output.innerText = "Hey the profit is "+ profit +" and profit% is "+ profitPercent.toFixed(2)+"%";
+        ctn.style.backgroundColor="#013a4c";
+
+    }
+    else if(market_price.value > selling_price.value)
+    {
+        var loss = ( market_price.value - selling_price.value )*quantity.value;
+        var lossPercent = (loss/selling_price.value)*100; 
+        
+        output.innerText ="Hey the loss is "+ loss +" and loss% is "+ lossPercent.toFixed(2)+"%";
+        ctn.style.backgroundColor="black";
+
+    }
+    else
+    {
+        output.style.display=" ";
+        output.innerText = "You're neither in loss nor in profit." 
+
+    }
+
+}
+
+
+
